a5a93aa0e9113817d6eea3d70528d2f7ad015c83
Hemanth912 <116891140+Hemanth912@users.noreply.github.com>
2022-12-25 17:24:56 +0530

    files added

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,11 +3,14 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
+        super(name,isManual);
         //Use arbitrary values for parameters which are not mentioned
+        this.setName(name);
+        this.setManual(isManual);
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed =this.getCurrentSpeed()+rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -20,8 +23,41 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            this.setGears(1);
+
         }
         //for all other cases, change the gear accordingly
+        if(newSpeed >=1 && newSpeed<=50) {
+            //Stop the car, set gear as 1
+            this.setGears(1);
+
+        }
+        if(newSpeed >=51 && newSpeed<=1000) {
+            //Stop the car, set gear as 1
+            this.setGears(2);
+
+        }if(newSpeed >=101 && newSpeed<=150) {
+            //Stop the car, set gear as 1
+            this.setGears(3);
+
+        }if(newSpeed >=151 && newSpeed<=2000) {
+            //Stop the car, set gear as 1
+            this.setGears(4);
+
+        }if(newSpeed >=201 && newSpeed<=250) {
+            //Stop the car, set gear as 1
+            this.setGears(5);
+
+        }
+        if(newSpeed >=250) {
+            //Stop the car, set gear as 1
+            this.setGears(6);
+
+        }
+
+
+
+
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
